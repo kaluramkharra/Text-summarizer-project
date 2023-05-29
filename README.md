4a22b7d77b2381fd660342dcea8fe45733e1d308
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-05-29 16:32:04 +0530

    sdsdsd

---
+++ README.md
@@ -0,0 +1,8 @@
+1 update config.yaml
+2. update params.yaml
+3 update entity
+4 update the configuration manager in src config
+5 update the components
+6 update pipeline
+7 update main.py
+8. update the app.py
--- src/textSummarizer/logging/__init__.py
+++ src/textSummarizer/logging/__init__.py
@@ -1,22 +1,22 @@
-import logging
 import os
-from datetime import datetime
+import sys
+import logging
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),'logs',LOG_FILE)
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
 
-os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    level= logging.INFO,
+    format= logging_str,
 
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
 )
 
 logger = logging.getLogger("textSummarizerLogger")
-
-# if __name__ == "__main__":
-#     logging.info("Logging is started")
